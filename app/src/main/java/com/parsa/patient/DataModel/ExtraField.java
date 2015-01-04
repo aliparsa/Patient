72ca6c65e71d1d8b98265edaefa7256586c5ef5e
ashkan ghodrat <ashkanpower@gmail.com>
2015-01-04 12:45:42 +0330

    data base created

--- app/src/main/java/com/parsa/patient/DataModel/ExtraField.java
+++ app/src/main/java/com/parsa/patient/DataModel/ExtraField.java
@@ -9,4 +9,44 @@ public class ExtraField {
     int parentId;
     String title;
     String value;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
