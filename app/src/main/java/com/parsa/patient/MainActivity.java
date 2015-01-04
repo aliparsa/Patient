677976e58488a41e85156ee110fe39e8f70dc1e5
parsa <ali.parsa.p@gmail.com>
2015-01-04 12:48:34 +0330

    for pull

--- app/src/main/java/com/parsa/patient/MainActivity.java
+++ app/src/main/java/com/parsa/patient/MainActivity.java
@@ -2,27 +2,33 @@ package com.parsa.patient;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity {
 
+    // Define Fields
+    ListView patientListView;
+    private android.content.Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_main);
+        context = this;
+        patientListView = new ListView(context);
+
     }
 
 
-    @Override
+/*    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
         return true;
-    }
+    }*/
 
-    @Override
+/*    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -32,5 +38,5 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
